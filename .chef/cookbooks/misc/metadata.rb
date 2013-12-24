44c6a45c6fd59a98f832d6831b0bd08574772066
Mike Barrett <mike182uk@gmail.com>
2013-12-24 16:07:56 +0000

    remove submodules

--- .chef/cookbooks/misc/metadata.rb
+++
@@ -1,9 +0,0 @@
-name              "custom"
-maintainer        "Mike Barrett"
-maintainer_email  "mike182uk@gmail.com"
-description       "Misc bootstrapping for a LAMP node"
-
-depends "php"
-
-recipe "custom::phpqatools", "Installs PHP QA Tools (PHPUnit, PHPMD, PHPCPD etc) via pear"
-recipe "custom::phpcsfixer", "Installs PHP CS Fixer"
